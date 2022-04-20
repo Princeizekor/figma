c48f625c202a4c196addce31d24891d43970c320
IZEKOR PAUL <izekorpaul297@gmail.com>
2022-04-20 12:15:27 +0100

    commit on 'main'

---
+++ hospot.js
@@ -0,0 +1,3 @@
+document.getElementById("navbar").style.backgroundColor = "white";
+document.getElementById("navbar").style.boxShadow = "0.5px 1px 3px 0.5px grey";
+document.getElementById("img").style.backgroundColor = "red";
--- js.js
+++ js.js
@@ -1,10 +1,40 @@
 document.getElementById("menu").innerHTML = "Hello World";
 document.getElementById("menu").style.color = "yellow";
 document.getElementById("menu").style.fontSize = "50px";
-alert(10 + 10);
+// document.getElementById('flat').style.float = "left";
+function myFunction() {
+    document.getElementById("plant").innerHTML = "Paragraph has been changed";
+}
+
+alert("This page is built by izekor on visual studio code,");
+
 console.log(15 + 15);
+
+document.getElementById('menu1').style.color = "grey";
 var x, y, z;
 var x = 10;
 var y = 10;
 var z = x + y;
 document.getElementById("sharp").innerHTML = "The value of z is " + z ;
+
+let text1 = "john";
+let text2 = "doe";
+let text3 = text1 +" "+ text2;
+document.getElementById("proud").innerHTML = text3;
+
+const myArray = [["people "], ["party "]];
+myArray.push(["tomorrow"], ["sweet"]);
+document.getElementById("Proud").innerHTML = myArray;
+
+const ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift();
+ourArray.unshift("Happy");
+document.getElementById("note").innerHTML = ourArray;
+
+// const myArray = [["people "], ["party "]];
+// myArray.pop(["tomorrow"], ["sweet"]);
+// document.getElementById("Proud").innerHTML = myArray;
+// const myArray = [["people "], ["party "]];
+// myArray.shift(["tomorrow"], ["sweet"]);
+// document.getElementById("Proud").innerHTML = myArray;
+
